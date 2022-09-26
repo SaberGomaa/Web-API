87817f5cd4dfb34c55bb626cd974fa203c771a81
sabegomaa261 <sabergomaa261@gmail.com>
2022-09-26 11:08:59 +0200

    Routing

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -18,11 +18,27 @@ namespace WebAPI.Controllers
             new Employee{Id = 14 , Name = "ahmed"}
         };
 
+
         [Route("api/emps")]
         [HttpGet]
         public List<Employee> AllEmployees()
         {
             return employees;
         }
+        
+        [HttpGet]
+        [Route("api/emps/{id:int}")]
+        public Employee employeeById(int id)
+        {
+            return employees.FirstOrDefault(e=>e.Id == id);
+        }
+
+        [HttpGet]
+        [Route("api/emps/{name:alpha}")]
+        public Employee employeeByName(string name)
+        {
+            return employees.FirstOrDefault(e => e.Name.ToLower() == name.ToLower());
+        }
+
     }
 }
