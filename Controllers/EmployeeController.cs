bbdf5b16dda32497f36480e50afca9bb26cbb27a
sabegomaa261 <sabergomaa261@gmail.com>
2022-09-26 10:02:06 +0200

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+
+        public List<Employee> getemployees()
+        {
+            return new List<Employee> { 
+                new Employee{Id = 12 , Name = "saber"},
+                new Employee{Id = 13 , Name = "maher"},
+                new Employee{Id = 14 , Name = "ahmed"}
+            };
+        }
+    }
+}
